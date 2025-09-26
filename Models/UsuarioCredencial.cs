a9c12b3582986f176681efbb1aa32f6560230465
dgaitan2025 <darwinnoez@gmail.com>
2025-09-26 11:39:57 -0600

    mime en model

--- Models/UsuarioCredencial.cs
+++ Models/UsuarioCredencial.cs
@@ -7,7 +7,9 @@ public class UsuarioCredencial
     public string? Telefono { get; set; }
     public string Nickname { get; set; } = string.Empty;
     public byte[]? Fotografia { get; set; }   // login (en BD)
+    public string? FotografiaMime { get; set; }   // Base64 para login
     public byte[]? Fotografia2 { get; set; }  // personalizada (en BD)
+    public string? Fotografia2Mime { get; set; }   // Base64 para login
     public string PasswordHash { get; set; } = string.Empty;
     public byte RolId { get; set; }
     public bool EstaActivo { get; set; }
