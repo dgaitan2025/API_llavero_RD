4d0abf67ddf8c86270d5d72bc33183e186817283
dgaitan2025 <darwinnoez@gmail.com>
2025-09-25 21:02:02 -0600

    v2.4

--- Models/Dto/LoginRequest.cs
+++ Models/Dto/LoginRequest.cs
@@ -2,6 +2,6 @@ namespace ProyDesaWeb2025.Models.Dto;
 
 public class LoginRequest
 {
-    public string Identificador { get; set; } = string.Empty; // Email, Teléfono o Nickname
-    public string Password { get; set; } = string.Empty;
+    public string usuario { get; set; } = string.Empty; // Email, Teléfono o Nickname
+    public string password { get; set; } = string.Empty;
 }
